51bcc9fdcd4b8109d62f442fbbe5416a136e1485
rootteamsawai <sawai@rootteam.co.jp>
2026-09-14 07:30:19 +0900

    backup snapshot 2026-09-14 of main (uncommitted work, auto-sweep)

--- otuge/seminar-digest/src/app/api/inngest/route.ts
+++
@@ -1,8 +0,0 @@
-import { serve } from "inngest/next";
-import { inngest } from "@/inngest/client";
-import { processVideo } from "@/inngest/functions/process-video";
-
-export const { GET, POST, PUT } = serve({
-  client: inngest,
-  functions: [processVideo],
-});
